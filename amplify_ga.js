b106f89a437a9ce85c79de9dc46d4130ff5253d5
Derick Chen <jockonarock@users.noreply.github.com>
2018-05-07 20:40:27 -0400

    Update amplify_ga.js

--- amplify_ga.js
+++ amplify_ga.js
@@ -1,6 +1,3 @@
-
-async src="https://www.googletagmanager.com/gtag/js?id=UA-117353439-1"
-
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
